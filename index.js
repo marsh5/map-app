ed06b26298a0e1d44c137ae896703fd7e58de115
marsh5 <brodiemarshall567@gmail.com>
2021-05-22 23:25:40 -0400

    displaying map

--- index.js
+++ index.js
@@ -10,3 +10,28 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+//check if not older browser
+if(navigator.geolocation){
+    navigator.geolocation.getCurrentPosition((position) =>{
+    //if succesful
+        const { latitude, longitude } = position.coords;
+        console.log(`https://www.google.com/maps/@${latitude},${longitude}`)
+
+        const coords = [latitude, longitude];
+
+        //leaflet library
+        const map = L.map('map').setView(coords, 13)
+
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map);
+
+        L.marker(coords).addTo(map)
+            .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+            .openPopup();
+                
+    }, () =>{
+        alert('Could not get your position')
+    })
+}
